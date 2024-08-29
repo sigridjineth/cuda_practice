7067a8ebdb7b59e30a6cc9084803e95e1f67b80a
sigridjineth <sigrid.jinhyung@gmail.com>
2024-08-29 14:48:49 +0900

    분석 1. 현재 half 정밀도를 사용 중이므로 GPU에서 이를 효율적으로 활용할 수 있어야 한다 2. Tensor 구조체의 메모리 레이아웃을 GPU에 최적화된 형태로 변경할 수 있음 - 연속적인 메모리 접근이 가능하도록 데이터를 재배치한다. 3. 연산 병렬화 - 각 레이어의 연산을 쿠다 커널로 구현하여 병렬화.

--- apss24/project/advanced/include/layer.h
+++ apss24/project/advanced/include/layer.h
@@ -1,5 +1,20 @@
 #pragma once
 
+/*
+ * layer.h 분석:
+
+다양한 딥러닝 연산들이 함수로 정의되어 있습니다.
+주요 연산들:
+
+요소별 연산: LeakyReLU, Tanh
+행렬 곱: Linear
+데이터 변형: Reshape
+컨볼루션 연산: ConvTranspose2d, Conv2d
+정규화: BatchNorm2d
+
+
+GPU 커널 예제로 LeakyReLU_cuda가 포함되어 있습니다.
+ */
 #include "tensor.h"
 
 /* Elementwise operations */
--- apss24/project/advanced/include/tensor.h
+++ apss24/project/advanced/include/tensor.h
@@ -13,7 +13,15 @@ typedef half_float::half half_cpu;
 using namespace half_float::literal; 
 
 
-/* [Tensor Structure] */
+/* tensor.h 분석:
+
+Tensor 구조체가 정의되어 있습니다.
+차원 수(ndim)와 shape(최대 4차원)을 저장합니다.
+데이터는 half_cpu (CPU용 half 정밀도 부동소수점) 타입의 포인터로 저장됩니다.
+Tensor 생성자와 소멸자가 정의되어 있습니다.
+num_elem() 함수로 총 원소 수를 계산할 수 있습니다.
+
+ */
 struct Tensor {
   size_t ndim = 0;
   size_t shape[4];
